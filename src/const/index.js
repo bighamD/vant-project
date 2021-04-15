0f08228aaa79c727eda1aa6def3c9661f6d9e807
bighamD <805359052@qq.com>
2021-04-16 01:02:45 +0800

    feat: weixin支付

--- src/const/index.js
+++ src/const/index.js
@@ -1,6 +1,3 @@
 export const APPID = 'wx6b7afa83a0cd7c0e';
 
-const REDIRECT_URI = encodeURIComponent(window.location.href);
-
-export const WX_AUTH_URI = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=snsapi_base&state=1234&connect_redirect=1#wechat_redirect`
-console.log(WX_AUTH_URI)
+export const WX_AUTH_URI = (uri, state) => `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${encodeURIComponent(uri)}&response_type=code&scope=snsapi_base&state=${state}&connect_redirect=1#wechat_redirect`
--- src/views/wx-pay/index.vue
+++ src/views/wx-pay/index.vue
@@ -1,18 +1,21 @@
-
+<template>
+    <div></div>
+</template>
 <script>
 import {wechatPay, } from '@/api';
 import {WX_AUTH_URI} from '@/const';
 export default {
     async created() {
-        const code = this.$route.query.code;
-        !code && (window.location.href = WX_AUTH_URI);
-        await this.reqWxPay();
+        const {code, state, id} = this.$route.query;
+        const redirect_uri = location.href.split('?')[0];
+        alert('code', code)
+        !code && (window.location.href = WX_AUTH_URI(redirect_uri, id));
+        await this.reqWxPay(code, state);
     },
     methods: {
-        async reqWxPay() {
-            const {id, code} = this.$route.query.id;
+        async reqWxPay(code, state) {
             await wechatPay({
-                id,
+                id: state,
                 code    
             })
         }
