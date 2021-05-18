e71c096cad465b9bbb3b21779d743a588b9e43a4
bighamD <805359052@qq.com>
2021-05-18 22:36:07 +0800

    ios機型處理vuex刷新失效

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import 'normalize.css/normalize.css';
-import Vue from 'vue';
-import Vant from 'vant';
-import 'vant/lib/index.css';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import 'amfe-flexible';
-import './style/global.less';
-import './perssion';
-import '@/const';
+import "normalize.css/normalize.css";
+import Vue from "vue";
+import Vant from "vant";
+import "vant/lib/index.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "amfe-flexible";
+import "./style/global.less";
+import "./perssion";
+import "@/const";
+import { isIOS } from "@/utils/platform";
 Vue.use(Vant);
 Vue.config.productionTip = false;
 
@@ -17,19 +18,21 @@ new Vue({
   store,
   render: h => h(App),
 
-  created () {
-    if (localStorage.getItem('store')) {
+  created() {
+    if (localStorage.getItem("store")) {
       this.$store.replaceState(
-        Object.assign(
-          {},
-          this.$store.state.userInfo,
-          JSON.parse(localStorage.getItem('store'))
-        )
+        Object.assign({}, JSON.parse(localStorage.getItem("store")))
       );
     }
-
-    window.addEventListener('beforeunload', () => {
-      localStorage.setItem('store', JSON.stringify(store.state));
-    });
+    if (isIOS) {
+      //在页面刷新时将vuex里的信息保存到缓存里
+      window.addEventListener("pagehide", () => {
+        localStorage.setItem("store", JSON.stringify(this.$store.state));
+      });
+    } else {
+      window.addEventListener("beforeunload", () => {
+        localStorage.setItem("store", JSON.stringify(this.$store.state));
+      });
+    }
   }
-}).$mount('#app');
+}).$mount("#app");
